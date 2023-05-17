dcb0bcadc75d6f2350b30e45e21e3635e45f4f5d
dilane3 <komboudilane125@gmail.com>
2023-05-18 00:35:35 +0100

    Add font and handling responsive for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -47,7 +47,6 @@ const Home: NextPage = () => {
             <Link href="/rooms/general/communique">
               <Button
                 style={{
-                  marginLeft: 20,
                   paddingInline: 30,
                   backgroundColor: Colors.primary,
                 }}
